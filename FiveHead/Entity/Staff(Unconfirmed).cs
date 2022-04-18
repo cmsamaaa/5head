f53b62fbb3e56c87464e547b6d02f38048375a1e
XJ <xijie629629@gmail.com>
2022-04-18 12:46:07 +0800

    added staff entity test code

---
+++ FiveHead/Entity/Staff(Unconfirmed).cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FiveHead.Entity
+{
+    public class Staff
+    {
+        private int accountID;
+        private int staffID;
+        private string staffName;
+
+        public Staff()
+        {
+
+        }
+
+
+        public Staff(int accountID)
+        {
+            this.accountID = accountID;
+        }
+
+        public Staff(int accountID, int staffID, string staffName) : this(accountID)
+        {
+            this.staffID = staffID;
+            this.staffName = staffName;
+        }
+
+        public int AccountID { get => accountID; set => accountID = value; }
+        public string StaffName { get => staffName; set => staffName = value; }
+        public int StaffID { get => staffID; set => staffID = value; }
+    }
+}
