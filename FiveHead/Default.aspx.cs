3669aebba78d0c594a3c981c62f4a09576f6303d
Loh Chun Mun <chunmun_loh@live.com>
2022-05-19 15:36:08 +0800

    added Default page for root directory

--- FiveHead/Default.aspx.cs
+++ FiveHead/Default.aspx.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace FiveHead
 {
@@ -11,7 +6,10 @@ namespace FiveHead
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                Response.Redirect("Menu.aspx", true);
+            }
         }
     }
 }
